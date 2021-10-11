b1ac64fcae96651578d8e272e168a9c7baa5bdab
jlbuild <juliabuildbot@gmail.com>
2021-10-11 21:59:30 +0000

    LLVM_jll build 13.0.0+0

--- src/wrappers/armv6l-linux-musleabihf-cxx11.jl
+++ src/wrappers/armv6l-linux-musleabihf-cxx11.jl
@@ -4,7 +4,7 @@ export liblto, llc, lld, llvm_mca, opt
 using Zlib_jll
 using libLLVM_jll
 JLLWrappers.@generate_wrapper_header("LLVM")
-JLLWrappers.@declare_library_product(liblto, "libLTO.so.12jl")
+JLLWrappers.@declare_library_product(liblto, "libLTO.so.13jl")
 JLLWrappers.@declare_executable_product(llc)
 JLLWrappers.@declare_executable_product(lld)
 JLLWrappers.@declare_executable_product(llvm_mca)
